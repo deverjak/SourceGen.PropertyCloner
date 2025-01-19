291d7d5dbfc1886e3c2290bfa652a4427c46313c
Jakub Devera <kuba.devera@seznam.cz>
2025-01-19 19:20:32 +0100

    Initial commit

---
+++ SourceGen.PropertyCloner.Example/Program.cs
@@ -0,0 +1,45 @@
+﻿using PropertyCloner;
+using MyNamespace;
+
+
+
+MyClass myClass = new() { Color = Color.Red, Size = Size.Large };
+var clone = myClass.Clone();
+clone.Color = Color.Green;
+
+namespace MyNamespace
+{
+    public enum Color
+    {
+        Red,
+        Green,
+        Blue,
+    }
+
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large,
+    }
+
+    public enum Shape
+    {
+        Circle,
+        Square,
+        Triangle,
+    }
+
+    public class BaseClass
+    {
+        [Clonable] public Shape Shape { get; set; }
+    }
+
+    [PropertyCloner]
+    public class MyClass : BaseClass
+    {
+        [Clonable] public Color Color { get; set; }
+        public Size Size { get; set; }
+    }
+}
+
